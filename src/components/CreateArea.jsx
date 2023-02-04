73b268a8b83c80ab02c8cefb11521b828594769b
Ayush <7126924ak@gmail.com>
2023-02-04 23:03:01 +0530

    inital files

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import { Fab, Zoom } from "@mui/material";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        {
+          isExpanded && (
+            <input
+              name="title"
+              onChange={handleChange}
+              value={note.title}
+              placeholder="Title"
+            />
+          )
+        }
+
+        <textarea
+          name="content"
+          onChange={handleChange}
+          onClick={() => { setIsExpanded(true) }}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isExpanded ? "3" : "1"}
+        />
+
+        <Zoom in={isExpanded}>
+          <Fab color="primary" onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
